c11871b30b073e0be9d8e3098b0e298255991e7d
Felipe Rezende <f.zende@gmail.com>
2023-03-31 23:34:10 +0200

    Chronometer done

---
+++ javascript/chronometer-centiseconds.js
@@ -0,0 +1,53 @@
+class Chronometer {
+    constructor() {
+      this.currentTime = 0;
+      this.intervalId = null;
+    }
+  
+    start(printTimeCallback) {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+  
+        if (printTimeCallback) {
+          printTimeCallback();
+        }
+      }, 10);
+    }
+  
+    getMinutes() {
+      let minutes = this.currentTime / 6000;
+      return Math.floor(minutes);
+    }
+  
+    getSeconds() {
+      let seconds = this.currentTime / 100;
+      return Math.floor(seconds) % 60;
+    }
+  
+    getCentiseconds() {
+      return this.currentTime % 100;
+    }
+  
+    computeTwoDigitNumber(value) {
+      if (value < 10) {
+        return `0${value}`;
+      } else {
+        return `${value}`;
+      }
+    }
+  
+    stop() {
+      clearInterval(this.intervalId);
+    }
+  
+    reset() {
+      this.currentTime = 0;
+    }
+  
+    split() {
+      let minutes = this.computeTwoDigitNumber(this.getMinutes());
+      let seconds = this.computeTwoDigitNumber(this.getSeconds());
+      let centiSeconds = this.computeTwoDigitNumber(this.getCentiseconds());
+      return `${minutes}:${seconds}.${centiSeconds}`;
+    }
+  }
--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,39 +1,47 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
-  start(callback) {
-    // ... your code goes here
+  start(printTimeCallback) {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = this.currentTime / 60;
+    return Math.floor(minutes);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return `0${value}`;
+    } else {
+      return `${value}`;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
   }
-}
-
-// The following is required to make unit tests work.
-/* Environment setup. Do not modify the below code. */
-if (typeof module !== 'undefined') {
-  module.exports = Chronometer;
 }
